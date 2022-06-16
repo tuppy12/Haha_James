bbf43f02961a177a8c8aff073b47f297cdbbc509
Tuppy12 <63974931+tuppy12@users.noreply.github.com>
2022-06-16 15:53:23 +0100

    Update dec_king.py

--- dec_king.py
+++ dec_king.py
@@ -11,7 +11,7 @@ os.system('termux-setup-storage')
 try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
-if 'arm' in arc:
+
         
 else:
         exit(f' Unknow device machine {arc}')
