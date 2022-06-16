cda6d43ce7f835d9205efabfca230dcda50317da
Tuppy12 <63974931+tuppy12@users.noreply.github.com>
2022-06-16 15:54:44 +0100

    Update dec_king.py

--- dec_king.py
+++ dec_king.py
@@ -2,12 +2,7 @@ import platform
 import os
 os.system('termux-setup-storage')
 #os.system('git pull')
-#try:os.system('mkdir /sdcard/PROHACK-DATA')
-#except:pass
-#try:os.system('mkdir /sdcard/PROHACK-DATA/OK')
-#except:pass
-#try:os.system('mkdir /sdcard/PROHACK-DATA/CP')
-#except:pass
+
 try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
