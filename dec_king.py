55fe0ce1d60308e554741bcc3751f3c250009ac8
Tuppy12 <63974931+tuppy12@users.noreply.github.com>
2022-06-16 15:57:40 +0100

    Update dec_king.py

--- dec_king.py
+++ dec_king.py
@@ -1,12 +1,4 @@
 import platform
 import os
-os.system('termux-setup-storage')
-#os.system('git pull')
-
-try:os.system('touch .prox.txt')
-except:pass
-arc = str(platform.uname().machine)
-
-        
-else:
-        exit(f' Unknow device machine {arc}')
+import tata1
+tata1.file()
