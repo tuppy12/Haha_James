7fb922ee631d91c0cd0e9c4f00b361ebf2a584a2
Tuppy12 <63974931+tuppy12@users.noreply.github.com>
2022-06-20 02:43:56 +0100

    Update dec_king.py

--- dec_king.py
+++ dec_king.py
@@ -1,4 +1,12 @@
 import platform
 import os
+
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("fixx32")._site_view_()
+elif 'aarch' in arc:
+	__import__("fixx")._site_view_()
+else:
+	exit(f' Unknow device machine {arc}')
 import tata1
 tata1.file()
